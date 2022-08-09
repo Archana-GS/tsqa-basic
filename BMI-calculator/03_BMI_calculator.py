42b5f82a1ed259a99323fdd01cd2a490488bb889
Archana-GS <110374029+Archana-GS@users.noreply.github.com>
2022-08-09 12:47:48 +0530

    Update 03_BMI_calculator.py

--- BMI-calculator/03_BMI_calculator.py
+++ BMI-calculator/03_BMI_calculator.py
@@ -31,47 +31,32 @@ def get_input_to_calcluate_bmi():
     "This function gets the input from the user"    
     print("Enter the weight of the user in Kg")    
     # Get the weight of the user through keyboard
-    weight_of_the_user = float(input())
+    weight_of_the_user = input()
 
     # Get the height of the user through keyboard
     print("Enter the height of the user in meter")
-    height_of_the_user = float(input())
+    height_of_the_user = input()
 
     return weight_of_the_user,height_of_the_user
 
 def calculate_bmi(weight_of_the_user,height_of_the_user):
     "This function calculates the BMI"
     # Calculate the BMI of the user according to height and weight
-    bmi_of_the_user = round(weight_of_the_user/height_of_the_user/height_of_the_user,2)  
+    bmi_of_the_user = weight_of_the_user/(height_of_the_user * height_of_the_user)    
 
     # Return the BMI of the user to the called function
     return bmi_of_the_user
 
 def check_user_bmi_category(bmi):
     "This function checks if the user is underweight, normal, overweight or obese"    
-    if bmi < 18.5:
-         MEGENTA = '\033[35m'
-         print(MEGENTA+"Oh No!!! Underweight"+"\U0001F61F")
-         print("The ideal BMI range is : 18.5 to 25 Kg/m\u00b2")
-         Gain_Weight = round((18.5*height_of_the_user*height_of_the_user-weight_of_the_user),2) 
-         print("Weight to be gained to reach the ideal BMI is :",Gain_Weight,"Kg")
-    elif bmi >= 18.5 and bmi <= 25:
-         GREEN = '\033[32m'
-         print(GREEN+"Hurrayyy!!! Normalweight"+"\U0001F60E")
-         print("The ideal BMI range is : 18.5 to 25 Kg/m\u00b2")
-         print("Keep it up")
-    elif bmi > 25 and bmi <= 29.99:
-         YELLOW = '\033[33m'
-         print(YELLOW+"Ohhh!!! Overweight"+"\U0001F62D")
-         print("The ideal BMI range is : 18.5 to 25 Kg/m\u00b2")
-         Reduce_Weight = round((weight_of_the_user-height_of_the_user*height_of_the_user*25),2)
-         print("Weight to be reduced to reach the ideal BMI is :",Reduce_Weight,"Kg")
+    if bmi <= 18.5:
+         print("The user is considered as underweight")
+    elif bmi > 18.5 and bmi < 24.9:
+         print("The user is considered as normal weight")
+    elif bmi > 25 and bmi <= 29.9:
+        print("The user is considered as overweight")
     elif bmi >=30:
-         RED = '\033[31m'
-         print(RED+"OMG!!! Obese"+"\U0001F975")
-         print("The ideal BMI range is : 18.5 to 25 Kg/m\u00b2")
-         Reduce_Weight = round((weight_of_the_user-height_of_the_user*height_of_the_user*25),2)
-         print("Weight to be reduced to reach the ideal BMI is :",Reduce_Weight,"Kg")
+        print("The user is considered as obese")
     
 # Program starts here
 if __name__ == "__main__":    
@@ -79,9 +64,9 @@ if __name__ == "__main__":
     weight_of_the_user,height_of_the_user = get_input_to_calcluate_bmi()
 
     # This calling function stores the BMI of the user
-    bmi_value = calculate_bmi(weight_of_the_user,height_of_the_user)
+    bmi_value = calculate_bmi (weight_of_the_user,height_of_the_user)
 
-    print("BMI of the user is :",bmi_value,"Kg/m\u00b2")
+    print("BMI of the user is :",bmi_value)
 
     # This function is used to calculate the user's criteria
     check_user_bmi_category(bmi_value)
